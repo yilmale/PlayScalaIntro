c48430ea2c5bb8f2461a387232f5a09fd0cb84f8
yilmaz <yilmaz@auburn.edu>
2015-11-01 22:15:34 -0600

    Updated controller for network updating and visualization

--- play-scala-intro/app/models/ModelParser.scala
+++ play-scala-intro/app/models/ModelParser.scala
@@ -36,6 +36,38 @@ import scala.collection.mutable.Map
 class ModelParser(model:String, g:Graph[String,Int] ) {
     var xmlModel : String = model
     var myG : Graph[String,Int] = g
+    var activations:Map[String,Double]  = Map()
+    var edgeWeights:Map[Int,Double] = Map()
+    val ACTIVATION = 0.01
+    
+     def graphIterator() {
+      var V = g.getVertices
+      var itr = V.iterator()
+      System.out.println("Vertex List: ")
+      while (itr.hasNext()) {
+        var myN = itr.next()
+        System.out.print("Node name: "+myN+ " ")
+        System.out.println("Activation: "+ activations(myN))
+         
+        var incIter = g.getIncidentEdges(myN).iterator()
+        while (incIter.hasNext()) {
+          var myEdge = incIter.next()
+          System.out.println("Incident edge number: "+ myEdge+" Weight is " + edgeWeights(myEdge) +" Source is "+g.getEndpoints(myEdge).getFirst+" Target is "+ g.getEndpoints(myEdge).getSecond)
+          System.out.println("The opposite vertex of"+myN+" in edge"+ myEdge+" is  "+ g.getOpposite(myN,myEdge))
+        }
+      }   
+    }
+    /*
+    def getNodes() : ListBuffer[String]  = {
+        var rt = new ListBuffer[String]
+        var nStr :String = null
+        var V = g.getVertices
+        var itr = V.iterator()
+        while (itr.hasNext()) {
+            var myN = itr.next()
+            nStr = 
+    }*/
+    
     def parse() {
         var dbFactory = DocumentBuilderFactory.newInstance()
         var dBuilder = dbFactory.newDocumentBuilder()
@@ -43,6 +75,184 @@ class ModelParser(model:String, g:Graph[String,Int] ) {
         var doc = dBuilder.parse(is)
         initializeCoherenceGraph()
         doc.getDocumentElement().normalize()
+        
+        var goalList = doc.getElementsByTagName("goal")
+        var myStr : String = null
+        for (a <- 0 to goalList.getLength()-1) {
+          var nNode = goalList.item(a)
+          var eElement = nNode.asInstanceOf[Element]
+          myStr = eElement.getElementsByTagName("name").item(0).getTextContent()
+          g.addVertex(myStr)
+          try {
+            var activationLevel = eElement.getElementsByTagName("activation").item(0).getTextContent()
+            activations+=(myStr -> activationLevel.toDouble)
+          
+          }
+          catch  {
+            case e: Exception => activations+=(myStr -> ACTIVATION)
+          }
+        }
+        var evidenceList = doc.getElementsByTagName("evidence")
+       
+        for (a <- 0 to evidenceList.getLength()-1) {
+          var nNode = evidenceList.item(a)
+          var eElement = nNode.asInstanceOf[Element]
+          myStr = eElement.getElementsByTagName("name").item(0).getTextContent()
+          g.addVertex(myStr)
+          try {
+            var activationLevel = eElement.getElementsByTagName("activation").item(0).getTextContent()
+            activations+=(myStr -> activationLevel.toDouble)
+          
+          }
+          catch  {
+            case e: Exception => activations+=(myStr -> ACTIVATION)
+          }
+        } 
+        
+        var beliefList = doc.getElementsByTagName("belief")
+       
+        for (a <- 0 to beliefList.getLength()-1) {
+          var nNode = beliefList.item(a)
+          var eElement = nNode.asInstanceOf[Element]
+          myStr = eElement.getElementsByTagName("name").item(0).getTextContent()
+          g.addVertex(myStr)
+          try {
+            var activationLevel = eElement.getElementsByTagName("activation").item(0).getTextContent()
+            activations+=(myStr -> activationLevel.toDouble)
+          
+          }
+          catch  {
+            case e: Exception => activations+=(myStr -> ACTIVATION)
+          }
+        }   
+        
+         var actionList = doc.getElementsByTagName("action")
+       
+        for (a <- 0 to actionList.getLength()-1) {
+          var nNode = actionList.item(a)
+          var eElement = nNode.asInstanceOf[Element]
+          myStr = eElement.getElementsByTagName("name").item(0).getTextContent()
+          g.addVertex(myStr)
+          try {
+            var activationLevel = eElement.getElementsByTagName("activation").item(0).getTextContent()
+            activations+=(myStr -> activationLevel.toDouble)
+          
+          }
+          catch  {
+            case e: Exception => activations+=(myStr -> ACTIVATION)
+          }
+        } 
+         
+         var sourceStr : String = null
+         var targetStr : String = null
+         var weight : Double = 0.0
+         var expConstraints = doc.getElementsByTagName("explain")
+         for (a <- 0 to expConstraints.getLength()-1) {
+           var sNode = expConstraints.item(a)
+           var eElement = sNode.asInstanceOf[Element]
+           sourceStr = new String(eElement.getElementsByTagName("source").item(0).getTextContent())
+           targetStr = new String(eElement.getElementsByTagName("target").item(0).getTextContent())
+           weight = eElement.getElementsByTagName("weight").item(0).getTextContent().toDouble
+           var edgeCount = g.getEdgeCount()
+           g.addEdge(g.getEdgeCount(),sourceStr,targetStr)
+           edgeWeights+=(edgeCount -> weight)   
+         }
+         
+         var deduceConstraints = doc.getElementsByTagName("deduce")
+         for (a <- 0 to deduceConstraints.getLength()-1) {
+           var sNode = deduceConstraints.item(a)
+           var eElement = sNode.asInstanceOf[Element]
+           sourceStr = new String(eElement.getElementsByTagName("source").item(0).getTextContent())
+           targetStr = new String(eElement.getElementsByTagName("target").item(0).getTextContent())
+           weight = eElement.getElementsByTagName("weight").item(0).getTextContent().toDouble
+           var edgeCount = g.getEdgeCount()
+           g.addEdge(g.getEdgeCount(),sourceStr,targetStr)
+           edgeWeights+=(edgeCount -> weight)           
+         }
+         
+         var facilitateConstraints = doc.getElementsByTagName("facilitate")
+         for (a <- 0 to facilitateConstraints.getLength()-1) {
+           var sNode = facilitateConstraints.item(a)
+           var eElement = sNode.asInstanceOf[Element]
+           sourceStr = new String(eElement.getElementsByTagName("source").item(0).getTextContent())
+           targetStr = new String(eElement.getElementsByTagName("target").item(0).getTextContent())
+           weight = eElement.getElementsByTagName("weight").item(0).getTextContent().toDouble
+           var edgeCount = g.getEdgeCount()
+           g.addEdge(g.getEdgeCount(),sourceStr,targetStr)
+           edgeWeights+=(edgeCount -> weight)          
+         }
+         
+         var triggerConstraints = doc.getElementsByTagName("trigger")
+         for (a <- 0 to triggerConstraints.getLength()-1) {
+           var sNode = triggerConstraints.item(a)
+           var eElement = sNode.asInstanceOf[Element]
+           sourceStr = new String(eElement.getElementsByTagName("source").item(0).getTextContent())
+           targetStr = new String(eElement.getElementsByTagName("target").item(0).getTextContent())
+           weight = eElement.getElementsByTagName("weight").item(0).getTextContent().toDouble
+           var edgeCount = g.getEdgeCount()
+           g.addEdge(g.getEdgeCount(),sourceStr,targetStr)
+           edgeWeights+=(edgeCount -> weight)           
+         }
+         
+         var inhibitConstraints = doc.getElementsByTagName("inhibit")
+         for (a <- 0 to inhibitConstraints.getLength()-1) {
+           var sNode = inhibitConstraints.item(a)
+           var eElement = sNode.asInstanceOf[Element]
+           sourceStr = new String(eElement.getElementsByTagName("source").item(0).getTextContent())
+           targetStr = new String(eElement.getElementsByTagName("target").item(0).getTextContent())
+           weight = 0-eElement.getElementsByTagName("weight").item(0).getTextContent().toDouble
+           var edgeCount = g.getEdgeCount()
+           g.addEdge(g.getEdgeCount(),sourceStr,targetStr)
+           edgeWeights+=(edgeCount -> weight)           
+         }
+         
+         var incompatibleConstraints = doc.getElementsByTagName("incompatible")
+         for (a <- 0 to incompatibleConstraints.getLength()-1) {
+           var sNode = incompatibleConstraints.item(a)
+           var eElement = sNode.asInstanceOf[Element]
+           sourceStr = new String(eElement.getElementsByTagName("source").item(0).getTextContent())
+           targetStr = new String(eElement.getElementsByTagName("target").item(0).getTextContent())
+           weight = 0-eElement.getElementsByTagName("weight").item(0).getTextContent().toDouble
+           var edgeCount = g.getEdgeCount()
+           g.addEdge(g.getEdgeCount(),sourceStr,targetStr)
+           edgeWeights+=(edgeCount -> weight)          
+         }
+         
+         var contradictConstraints = doc.getElementsByTagName("contradict")
+         for (a <- 0 to contradictConstraints.getLength()-1) {
+           var sNode = contradictConstraints.item(a)
+           var eElement = sNode.asInstanceOf[Element]
+           sourceStr = new String(eElement.getElementsByTagName("source").item(0).getTextContent())
+           targetStr = new String(eElement.getElementsByTagName("target").item(0).getTextContent())
+           weight = 0-eElement.getElementsByTagName("weight").item(0).getTextContent().toDouble
+           var edgeCount = g.getEdgeCount()
+           g.addEdge(g.getEdgeCount(),sourceStr,targetStr)
+           edgeWeights+=(edgeCount -> weight)          
+         }
+         
+         var similarConstraints = doc.getElementsByTagName("similar")
+         for (a <- 0 to similarConstraints.getLength()-1) {
+           var sNode = similarConstraints.item(a)
+           var eElement = sNode.asInstanceOf[Element]
+           sourceStr = new String(eElement.getElementsByTagName("source").item(0).getTextContent())
+           targetStr = new String(eElement.getElementsByTagName("target").item(0).getTextContent())
+           weight = eElement.getElementsByTagName("weight").item(0).getTextContent().toDouble
+           var edgeCount = g.getEdgeCount()
+           g.addEdge(g.getEdgeCount(),sourceStr,targetStr)
+           edgeWeights+=(edgeCount -> weight)           
+         }
+         
+         var competeConstraints = doc.getElementsByTagName("compete")
+         for (a <- 0 to competeConstraints.getLength()-1) {
+           var sNode = competeConstraints.item(a)
+           var eElement = sNode.asInstanceOf[Element]
+           sourceStr = new String(eElement.getElementsByTagName("source").item(0).getTextContent())
+           targetStr = new String(eElement.getElementsByTagName("target").item(0).getTextContent())
+           weight = 0-eElement.getElementsByTagName("weight").item(0).getTextContent().toDouble
+           var edgeCount = g.getEdgeCount()
+           g.addEdge(g.getEdgeCount(),sourceStr,targetStr)
+           edgeWeights+=(edgeCount -> weight)           
+         }
     }
     
     def initializeCoherenceGraph() {
@@ -62,4 +272,3 @@ class ModelParser(model:String, g:Graph[String,Int] ) {
     }
     
 }
-
---
+++ play-scala-intro/app/models/PersonController.scala
@@ -0,0 +1,277 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.i18n._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import play.api.libs.json.Json
+import models._
+import dal._
+import play.api.libs.json.JsValue
+
+import play.api.libs.json._
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+import javax.inject._
+
+import schellingModel._
+
+import edu.uci.ics.jung.algorithms.layout.AbstractLayout
+import edu.uci.ics.jung.algorithms.layout.FRLayout2
+import edu.uci.ics.jung.algorithms.layout.util.Relaxer
+import edu.uci.ics.jung.graph.UndirectedSparseMultigraph
+import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
+import edu.uci.ics.jung.graph.Graph
+import edu.uci.ics.jung.graph.ObservableGraph
+import edu.uci.ics.jung.graph.util.Graphs
+import edu.uci.ics.jung.visualization.VisualizationViewer
+import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller
+import edu.uci.ics.jung.visualization.renderers.Renderer
+
+import scala.collection.mutable.ListBuffer
+
+case class Location(lat: Double, long: Double)
+case class Resident(name: String, age: Int, role: Option[String])
+case class MRow(name: String, edgeWeights: Seq[Double])
+case class AdjMatrix(name: String, nodes: Seq[String], adjList: Seq[MRow])
+case class Place(name: String, location: Location, residents: Seq[Resident])
+
+class PersonController @Inject() (repo: PersonRepository, val messagesApi: MessagesApi)
+                                 (implicit ec: ExecutionContext) extends Controller with I18nSupport{
+  var g: Graph[String,Int] = null;
+  var og: ObservableGraph[String,Int] =null;
+  var ig = Graphs.synchronizedUndirectedGraph[String,Int](new UndirectedSparseMultigraph[String,Int]())
+  og = new ObservableGraph[String,Int](ig)   
+  g = og
+  
+  val ACTIVATION = 0.01
+  val ACTIVATIONTHRESHOLD = 0.20
+  val INITIALTHRESHOLD = 0.5
+  val CTHRESHOLD = 0.001 
+  val MAXITERATION = 200
+  val MAX = 1.0
+  val MIN = -1.0
+  val DECAYRATE = 0.05
+  var activations:Map[String,Double]  = Map()
+  var edgeWeights:Map[Int,Double] = Map()
+  
+  /**
+   * The mapping for the person form.
+   */
+  val personForm: Form[CreatePersonForm] = Form {
+    mapping(
+      "name" -> nonEmptyText,
+      "age" -> number.verifying(min(0), max(140))
+    )(CreatePersonForm.apply)(CreatePersonForm.unapply)
+  }
+  
+  implicit val locationWrites = new Writes[Location] {
+  def writes(location: Location) = Json.obj(
+    "lat" -> location.lat,
+    "long" -> location.long
+  )
+}
+  
+  implicit val mrowWrites = new Writes[MRow] {
+      def writes(mrow: MRow) = Json.obj(
+        "name" -> mrow.name,
+        "edgeWeights" -> mrow.edgeWeights
+      )
+  }
+  
+  implicit val adjmatrixWrites = new Writes[AdjMatrix] {
+      def writes(adjmatrix: AdjMatrix) = Json.obj(
+        "name" -> adjmatrix.name,
+        "nodes" -> adjmatrix.nodes,
+        "adjList" -> adjmatrix.adjList
+      )
+  }
+  
+  implicit val residentWrites = new Writes[Resident] {
+  def writes(resident: Resident) = Json.obj(
+    "name" -> resident.name,
+    "age" -> resident.age,
+    "role" -> resident.role
+  )
+}
+
+implicit val placeWrites = new Writes[Place] {
+  def writes(place: Place) = Json.obj(
+    "name" -> place.name,
+    "location" -> place.location,
+    "residents" -> place.residents)
+}
+
+
+  
+  
+  def initializeGraphModel() {
+      if (g.getVertexCount>0) {
+            var V = g.getVertices
+            var vertices=V.toArray()
+            for (i <- 0 to g.getVertexCount-1) {
+                g.removeVertex(vertices(i).asInstanceOf[String])
+            }
+         } 
+        var eC= g.getEdgeCount()
+        if (eC>0) {
+            for (i <- 0 to eC-1) {
+                g.removeEdge(i)
+            }
+        }
+  }
+
+  /**
+   * The index action.
+   */
+  def index = Action {
+    Ok(views.html.index(personForm))
+  }
+
+  /**
+   * The add person action.
+   *
+   * This is asynchronous, since we're invoking the asynchronous methods on PersonRepository.
+   */
+  def addPerson = Action.async { implicit request =>
+    // Bind the form first, then fold the result, passing a function to handle errors, and a function to handle succes.
+    personForm.bindFromRequest.fold(
+      // The error function. We return the index page with the error form, which will render the errors.
+      // We also wrap the result in a successful future, since this action is synchronous, but we're required to return
+      // a future because the person creation function returns a future.
+      errorForm => {
+        Future.successful(Ok(views.html.index(errorForm)))
+      },
+      // There were no errors in the from, so create the person.
+      person => {
+        repo.create(person.name, person.age).map { _ =>
+          // If successful, we simply redirect to the index page.
+          Redirect(routes.PersonController.index)
+        }
+      }
+    )
+  }
+  
+  def getEntities = Action { 
+  Ok("Got request [" + "]")
+}
+
+
+/*def addEntity = Action { implicit request =>
+  val body: AnyContent = request.body
+  val textBody: Option[String] = body.asText 
+  textBody.map { text =>
+    Ok("Got: " + text)
+  }.getOrElse {
+    BadRequest("Expecting text/plain request body")  
+  }
+  
+}*/
+
+
+def addEntity = Action { implicit request =>
+  System.out.println(request.body)
+  val name = request.body.asFormUrlEncoded.get("name")
+  
+  System.out.println(name)
+  System.out.println(name.length)
+  System.out.println(name(0))  
+  var mystr:String = new String(name(0))
+  System.out.println("Derived string is " + mystr)
+  val age = request.body.asFormUrlEncoded.get("age[0][]")
+  System.out.println(age)
+  //Ok("Got request [" + request.body + "]")
+  //var pt : Point = new Point()
+  val myStock = new Stock("GOOG", 650.0)
+  Ok(Json.toJson(myStock))
+}
+
+def addEntityJSON = Action { implicit request =>
+  System.out.println(request.body)
+  val myModel = request.body.asFormUrlEncoded.get("model")
+  var mystr:String = new String(myModel(0))
+  System.out.println("Received model specification is " + mystr)
+  
+  initializeGraphModel()
+  var mParser = new ModelParser(mystr,g)
+  mParser.parse()
+  mParser.graphIterator()
+  
+  val place = Place(
+  "Watership Down",
+  Location(51.235685, -1.309197),
+  Seq(
+    Resident("Fiver", 4, None),
+    Resident("Bigwig", 6, Some("Owsla"))
+  )
+)
+
+  val myNet  = AdjMatrix(
+    "TestNetwork",
+    Seq(
+      "N1",
+      "N2",
+      "N3"
+    ),
+    Seq(
+      MRow("N1",Seq(0.0, 0.5, 0.3)),
+      MRow("N2",Seq(0.0, 0.0, 0.5)),
+      MRow("N3",Seq(0.0, 0.0, 0.0))
+    )
+    )
+  var json = Json.toJson(place)
+  
+  val age = request.body.asFormUrlEncoded.get("age[0][]")
+  System.out.println(age)
+  val myStock = new Stock("GOOG", 650.0)
+  //Ok(Json.toJson(myStock))
+  
+  
+  var nodeList = new ListBuffer[String]()
+  
+  nodeList += myNet.nodes(0)
+  nodeList += myNet.nodes(1)
+  nodeList += myNet.nodes(2)
+  
+  var rowList = new ListBuffer[MRow]()
+  
+  var row1 = MRow(myNet.nodes(0),myNet.adjList(0).edgeWeights)
+  var row2 = MRow(myNet.nodes(1),myNet.adjList(1).edgeWeights)
+  var row3 = MRow(myNet.nodes(2),myNet.adjList(2).edgeWeights)
+  
+  rowList+=row1
+  rowList+=row2
+  rowList+=row3
+  
+  var myNet1 = new AdjMatrix("TestNetwork",nodeList, rowList)
+  
+   Ok(Json.toJson(myNet1))
+}
+
+def getEntityJSONs = Action {
+    val myStock = new Stock("GOOG", 650.0)
+    Ok(Json.toJson(myStock))
+  }
+
+  /**
+   * A REST endpoint that gets all the people as JSON.
+   */
+  def getPersons = Action.async {
+    repo.list().map { people =>
+      Ok(Json.toJson(people))
+    }
+  }
+}
+
+/**
+ * The create person form.
+ *
+ * Generally for forms, you should define separate objects to your models, since forms very often need to present data
+ * in a different way to your models.  In this case, it doesn't make sense to have an id parameter in the form, since
+ * that is generated once it's created.
+ */
+case class CreatePersonForm(name: String, age: Int)
