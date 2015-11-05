40cd4900a6bf46122e334e4ba1eff95e688d54a0
yilmale <yilmaz@auburn.edu>
2015-11-05 17:26:47 -0600

    Simplified the interface and the controller

--- play-scala-intro/app/models/PersonController.scala
+++ play-scala-intro/app/models/PersonController.scala
@@ -39,7 +39,7 @@ case class MRow(name: String, edgeWeights: Seq[Double])
 case class AdjMatrix(name: String, nodes: Seq[String], activations: Seq[Double], adjList: Seq[MRow])
 case class Place(name: String, location: Location, residents: Seq[Resident])
 
-class PersonController @Inject() (repo: PersonRepository, val messagesApi: MessagesApi)
+class PersonController  @Inject() (val messagesApi: MessagesApi)
                                  (implicit ec: ExecutionContext) extends Controller with I18nSupport{
   var g: Graph[String,Int] = null;
   var og: ObservableGraph[String,Int] =null;
@@ -129,7 +129,7 @@ implicit val placeWrites = new Writes[Place] {
    * The index action.
    */
   def index = Action {
-    Ok(views.html.index(personForm))
+    Ok(views.html.index())
   }
 
   /**
@@ -137,24 +137,6 @@ implicit val placeWrites = new Writes[Place] {
    *
    * This is asynchronous, since we're invoking the asynchronous methods on PersonRepository.
    */
-  def addPerson = Action.async { implicit request =>
-    // Bind the form first, then fold the result, passing a function to handle errors, and a function to handle succes.
-    personForm.bindFromRequest.fold(
-      // The error function. We return the index page with the error form, which will render the errors.
-      // We also wrap the result in a successful future, since this action is synchronous, but we're required to return
-      // a future because the person creation function returns a future.
-      errorForm => {
-        Future.successful(Ok(views.html.index(errorForm)))
-      },
-      // There were no errors in the from, so create the person.
-      person => {
-        repo.create(person.name, person.age).map { _ =>
-          // If successful, we simply redirect to the index page.
-          Redirect(routes.PersonController.index)
-        }
-      }
-    )
-  }
   
   def getEntities = Action { 
   Ok("Got request [" + "]")
@@ -282,22 +264,5 @@ def getEntityJSONs = Action {
     val myStock = new Stock("GOOG", 650.0)
     Ok(Json.toJson(myStock))
   }
-
-  /**
-   * A REST endpoint that gets all the people as JSON.
-   */
-  def getPersons = Action.async {
-    repo.list().map { people =>
-      Ok(Json.toJson(people))
-    }
-  }
 }
-
-/**
- * The create person form.
- *
- * Generally for forms, you should define separate objects to your models, since forms very often need to present data
- * in a different way to your models.  In this case, it doesn't make sense to have an id parameter in the form, since
- * that is generated once it's created.
- */
 case class CreatePersonForm(name: String, age: Int)
